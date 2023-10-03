33e8848a5ae83603ca4a0c454fc2d77c7ba1b9fe
dominique-hillebrand <dominique.hillebrand@gmail.com>
2023-10-03 12:24:24 -0500

    show fotos from houses in cardHouses

--- components/CardHouses.tsx
+++ components/CardHouses.tsx
@@ -10,14 +10,89 @@ import { cookies } from "next/headers";
 export default async function CardHouses() {
   const houses = await allHouses();
 
+  // console.log(houses);
+
+  const fotosFromHouses = await Promise.all(
+    houses?.map(async (house) => {
+      // console.log(house.profiles.id);
+      const urls = await listAllFileUrlsFromBucket(house.profiles.id);
+      return { ...house, fotos: urls };
+
+      async function listAllFileUrlsFromBucket(id) {
+        const supabase = createServerComponentClient({ cookies });
+        try {
+          const { data, error } = await supabase.storage
+            .from("houses")
+            .list(id, {
+              limit: 100,
+              offset: 0,
+              sortBy: { column: "name", order: "asc" },
+            });
+          if (error) {
+            console.error("Error listing files:", error.message);
+            return [];
+          }
+          // console.log(data);
+          const urls = await publicUrls(data);
+          return urls;
+        } catch (error) {
+          console.error("An error occurred:", error.message);
+          return [];
+        }
+
+        async function publicUrls(data) {
+          const urls = await Promise.all(
+            data.map(async (file) => {
+              try {
+                const { data: fileData, error: fileError } = supabase.storage
+                  .from("houses")
+                  .getPublicUrl(`${id}/${file.name}`);
+
+                if (fileError) {
+                  console.error(
+                    "Error getting public URL for file:",
+                    file.name,
+                    fileError.message
+                  );
+                  return null;
+                }
+                // console.log(fileData.publicUrl);
+                // console.log("is this array", fileData);
+                return fileData.publicUrl;
+              } catch (error) {
+                console.error("An error occurred:", error.message);
+                return null;
+              }
+            })
+          );
+          const filteredUrls = urls.filter((url) => url !== null);
+          // console.log("All public URLs❤️:", filteredUrls);
+
+          return filteredUrls;
+        }
+      }
+    })
+  );
+  console.log(fotosFromHouses);
   return (
     <>
-      {houses?.map((house) => {
+      {fotosFromHouses?.map((house) => {
         const profile = house.profiles;
         return (
           <div key={house.id} className="box-shadow1 rounded-xl p-4">
             <p className="text-2xl">{house.name}</p>
-            <p>{house.foto}</p>
+            {house.fotos.length > 0 && (
+              <div className="flex">
+                {house.fotos.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`Image ${index}`}
+                    style={{ width: "100px", height: "100px", margin: "5px" }}
+                  />
+                ))}
+              </div>
+            )}
             <br />
             <p>Description:</p>
             <p className="h-14">{house.description}</p>
