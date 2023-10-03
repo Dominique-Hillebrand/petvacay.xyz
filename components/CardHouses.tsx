f921444ce2a311cc21579c9a127c98d29d9109a8
dominique-hillebrand <dominique.hillebrand@gmail.com>
2023-10-03 14:49:18 -0500

    show fotos from houses and persons in cardHouses

--- components/CardHouses.tsx
+++ components/CardHouses.tsx
@@ -10,97 +10,241 @@ import { cookies } from "next/headers";
 export default async function CardHouses() {
   const houses = await allHouses();
 
-  // console.log(houses);
+  // ++++++  SHOW ALL FOTOS FROM ALL HOUSES ++++++++++ working! :)
+  // const fotosFromHouses = await Promise.all(
+  //   houses?.map(async (house) => {
+  //     // console.log(house.profiles.id);
+  //     const urls = await listAllFileUrlsFromBucket(house.profiles.id);
+  //     return { ...house, fotos: urls };
 
-  const fotosFromHouses = await Promise.all(
+  //     async function listAllFileUrlsFromBucket(id) {
+  //       const supabase = createServerComponentClient({ cookies });
+  //       try {
+  //         const { data, error } = await supabase.storage
+  //           .from("houses")
+  //           .list(id, {
+  //             limit: 100,
+  //             offset: 0,
+  //             sortBy: { column: "name", order: "asc" },
+  //           });
+  //         if (error) {
+  //           console.error("Error listing houses files:", error.message);
+  //           return [];
+  //         }
+  //         // console.log(data);
+  //         const urls = await publicUrls(data);
+  //         return urls;
+  //       } catch (error) {
+  //         console.error("An error occurred:", error.message);
+  //         return [];
+  //       }
+
+  //       async function publicUrls(data) {
+  //         const urls = await Promise.all(
+  //           data.map(async (file) => {
+  //             try {
+  //               const { data: fileData, error: fileError } = supabase.storage
+  //                 .from("houses")
+  //                 .getPublicUrl(`${id}/${file.name}`);
+
+  //               if (fileError) {
+  //                 console.error(
+  //                   "Error getting public URL for houses file:",
+  //                   file.name,
+  //                   fileError.message
+  //                 );
+  //                 return null;
+  //               }
+  //               // console.log(fileData.publicUrl);
+  //               // console.log("is this array", fileData);
+  //               return fileData.publicUrl;
+  //             } catch (error) {
+  //               console.error("An error occurred:", error.message);
+  //               return null;
+  //             }
+  //           })
+  //         );
+  //         const filteredUrls = urls.filter((url) => url !== null);
+  //         // console.log("All public URLs❤️:", filteredUrls);
+
+  //         return filteredUrls;
+  //       }
+  //     }
+  //   })
+  // );
+
+  //**********SHOW ALL FOTOS FROM SITTER */  not sure if working but should be
+  // const fotosFromPersons = await Promise.all(
+  //   houses?.map(async (house) => {
+  //     // console.log(house.profiles.id);
+  //     const urls = await listAllFileUrlsFromBucket(house.profiles.id);
+  //     return { ...house, fotos: urls };
+
+  //     async function listAllFileUrlsFromBucket(id) {
+  //       const supabase = createServerComponentClient({ cookies });
+  //       try {
+  //         const { data, error } = await supabase.storage
+  //           .from("avatars")
+  //           .list(id, {
+  //             limit: 100,
+  //             offset: 0,
+  //             sortBy: { column: "name", order: "asc" },
+  //           });
+  //         if (error) {
+  //           console.error("Error listing avatar files:", error.message);
+  //           return [];
+  //         }
+  //         // console.log(data);
+  //         const urls = await publicUrls(data);
+  //         return urls;
+  //       } catch (error) {
+  //         console.error("An error occurred:", error.message);
+  //         return [];
+  //       }
+
+  //       async function publicUrls(data) {
+  //         const urls = await Promise.all(
+  //           data.map(async (file) => {
+  //             try {
+  //               const { data: fileData, error: fileError } = supabase.storage
+  //                 .from("avatars")
+  //                 .getPublicUrl(`${id}/${file.name}`);
+
+  //               if (fileError) {
+  //                 console.error(
+  //                   "Error getting public URL for avatar file:",
+  //                   file.name,
+  //                   fileError.message
+  //                 );
+  //                 return null;
+  //               }
+  //               // console.log(fileData.publicUrl);
+  //               // console.log("is this array", fileData);
+  //               return fileData.publicUrl;
+  //             } catch (error) {
+  //               console.error("An error occurred:", error.message);
+  //               return null;
+  //             }
+  //           })
+  //         );
+  //         const filteredUrls = urls.filter((url) => url !== null);
+  //         // console.log("All public URLs❤️:", filteredUrls);
+
+  //         return filteredUrls;
+  //       }
+  //     }
+  //   })
+  // );
+
+  const fotos = await Promise.all(
     houses?.map(async (house) => {
-      // console.log(house.profiles.id);
-      const urls = await listAllFileUrlsFromBucket(house.profiles.id);
-      return { ...house, fotos: urls };
-
-      async function listAllFileUrlsFromBucket(id) {
-        const supabase = createServerComponentClient({ cookies });
-        try {
-          const { data, error } = await supabase.storage
-            .from("houses")
-            .list(id, {
-              limit: 100,
-              offset: 0,
-              sortBy: { column: "name", order: "asc" },
-            });
-          if (error) {
-            console.error("Error listing files:", error.message);
-            return [];
-          }
-          // console.log(data);
-          const urls = await publicUrls(data);
-          return urls;
-        } catch (error) {
-          console.error("An error occurred:", error.message);
-          return [];
-        }
-
-        async function publicUrls(data) {
-          const urls = await Promise.all(
-            data.map(async (file) => {
-              try {
-                const { data: fileData, error: fileError } = supabase.storage
-                  .from("houses")
-                  .getPublicUrl(`${id}/${file.name}`);
-
-                if (fileError) {
-                  console.error(
-                    "Error getting public URL for file:",
-                    file.name,
-                    fileError.message
-                  );
-                  return null;
-                }
-                // console.log(fileData.publicUrl);
-                // console.log("is this array", fileData);
-                return fileData.publicUrl;
-              } catch (error) {
-                console.error("An error occurred:", error.message);
-                return null;
-              }
-            })
-          );
-          const filteredUrls = urls.filter((url) => url !== null);
-          // console.log("All public URLs❤️:", filteredUrls);
-
-          return filteredUrls;
-        }
-      }
+      const houseUrls = await listAllFileUrlsFromBucket(
+        "houses",
+        house.profiles.id
+      );
+      const profileUrls = await listAllFileUrlsFromBucket(
+        "avatars",
+        house.profiles.id
+      );
+      return { ...house, houseFotos: houseUrls, profileFotos: profileUrls };
     })
   );
-  console.log(fotosFromHouses);
+
+  async function listAllFileUrlsFromBucket(bucketName, id) {
+    const supabase = createServerComponentClient({ cookies });
+
+    try {
+      const { data, error } = await supabase.storage.from(bucketName).list(id, {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" },
+      });
+      if (error) {
+        console.error("Error listing files:", error.message);
+        return [];
+      }
+      const urls = await publicUrls(data);
+      return urls;
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+      return [];
+    }
+
+    async function publicUrls(data) {
+      const urls = await Promise.all(
+        data.map(async (file) => {
+          try {
+            const { data: fileData, error: fileError } = supabase.storage
+              .from(bucketName)
+              .getPublicUrl(`${id}/${file.name}`);
+
+            if (fileError) {
+              console.error(
+                "Error getting public URL for file:",
+                file.name,
+                fileError.message
+              );
+              return null;
+            }
+
+            return fileData.publicUrl;
+          } catch (error) {
+            console.error("An error occurred:", error.message);
+            return null;
+          }
+        })
+      );
+      const filteredUrls = urls.filter((url) => url !== null);
+      return filteredUrls;
+    }
+  }
   return (
     <>
-      {fotosFromHouses?.map((house) => {
+      {fotos?.map((house) => {
         const profile = house.profiles;
         return (
           <div key={house.id} className="box-shadow1 rounded-xl p-4">
-            <p className="text-2xl">{house.name}</p>
-            {house.fotos.length > 0 && (
+            <p className="text-6xl">{house.name}</p>
+            {house.houseFotos && house.houseFotos.length > 0 && (
               <div className="flex">
-                {house.fotos.map((url, index) => (
+                {house.houseFotos.map((url, index) => (
                   <img
-                    key={index}
+                    key={`house-${index}`}
                     src={url}
-                    alt={`Image ${index}`}
+                    alt={`House Image ${index}`}
                     style={{ width: "100px", height: "100px", margin: "5px" }}
                   />
                 ))}
               </div>
             )}
+            {/* {!house.houseFotos ||
+              (house.houseFotos.length === 0 && (
+                <p>No house photos available.</p>
+              ))} */}
             <br />
             <p>Description:</p>
             <p className="h-14">{house.description}</p>
             <p>{house.m2} m2</p>
             <br />
+            <p>Sitter Details:</p>
+            {house.profileFotos && house.profileFotos.length > 0 && (
+              <div className="flex">
+                {house.profileFotos.map((url, index) => (
+                  <img
+                    key={`profile-${index}`}
+                    src={url}
+                    alt={`Profile Image ${index}`}
+                    style={{ width: "100px", height: "100px", margin: "5px" }}
+                  />
+                ))}
+              </div>
+            )}
+            {/* {!house.profileFotos ||
+              (house.profileFotos.length === 0 && (
+                <p>No profile photos available.</p>
+              ))} */}
             {profile && (
               <>
-                <p>Sitter Details:</p>
                 <p>
                   {profile.first_name} {profile.last_name}
                 </p>
